362de5118f7810dbeb136800adb2ae839b95f70e
mahmoudfathy557 <mahmoudfathy557@gmail.com>
2022-05-05 20:42:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# nodejs-jwt-cookies
---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
+const { requireAuth, checkUser } = require('./middleware/authMiddleware')
+
+const app = express()
+
+// middleware
+app.use(express.static('public'))
+app.use(express.json())
+app.use(cookieParser())
+
+// view engine
+app.set('view engine', 'ejs')
+
+// database connection
+const dbURI =
+  'mongodb+srv://test:test@todo.waot9.mongodb.net/admin-panel?retryWrites=true&w=majority'
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) =>
+    app.listen(4000, () => console.log('app is listening to 4000'))
+  )
+  .catch((err) => console.log(err))
+
+// routes
+app.get('*', checkUser)
+app.get('/', (req, res) => res.render('home'))
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'))
+app.use(authRoutes)
